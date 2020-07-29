9bed72ce0b978fd9b45acb87a63713fe25e922ce
Peter Stoenchev <peter.stoenchev@abv.bg>
2020-07-29 19:00:26 +0300

    Initial Commit

---
+++ StringWork/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StringWork
+//
+//  Created by Mac on 29.07.20.
+//  Copyright © 2020 peter. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
